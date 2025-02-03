a39347493116e47ab6fce705652d40d29f41a9ce
ljhvvs1181 <a2775269@gmail.com>
2025-02-03 17:26:03 +0800

    feature: 新增刪除錢包API

--- app/api/users/wallet/route.ts
+++ app/api/users/wallet/route.ts
@@ -258,3 +258,82 @@ export async function PATCH(request: NextRequest): Promise<NextResponse> {
     }
   }
 }
+
+export async function DELETE(request: NextRequest) {
+  const authHeader = request.headers.get("Authorization");
+  if (!authHeader) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Authorization header is required",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const jwtData = await verifyToken(token);
+    await connectDB();
+    const data = await request.json();
+    const { id } = data;
+    if (!id) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Missing required fields",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    const deletedWallet = await Wallet.findOneAndDelete({
+      _id: id,
+      userId: jwtData.uuid,
+    });
+    if (!deletedWallet) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Wallet not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Wallet deleted successfully",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: err.message,
+        },
+        {
+          status: 400,
+        }
+      );
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+          message: err,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+}
