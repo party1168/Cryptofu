87d6395ab8af83801abe286f36e53c87539c744a
ljhvvs1181 <a2775269@gmail.com>
2025-02-11 18:31:29 +0800

    feature: 新增查詢Max交易紀錄API

---
+++ app/api/exchange/max/history/route.ts
@@ -0,0 +1,76 @@
+import { NextRequest, NextResponse } from "next/server";
+import { decryptAES } from "@/lib/rijindael";
+import Exchange from "@/models/Exchange";
+import getExchangeMaxHistory from "@/lib/getMaxHistory";
+import { verifyToken } from "@/lib/auth";
+
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("Authorization");
+  if (!authHeader) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Authorization header is required",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const jwtData = await verifyToken(token);
+    const userExchange = await Exchange.findOne({
+      userId: jwtData.uuid,
+      name: "Max",
+    });
+    if (!userExchange) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "No exchange found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+    const decryptedAPIkey = decryptAES(userExchange.APIkey);
+    const decryptedAPIsecret = decryptAES(userExchange.APIsecret);
+    const data = await getExchangeMaxHistory(
+      decryptedAPIkey,
+      decryptedAPIsecret
+    );
+    return NextResponse.json(
+      {
+        success: true,
+        data,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: err.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+          message: err,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+}
