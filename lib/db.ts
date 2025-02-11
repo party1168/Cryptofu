209dfb8b662cbab04edef19a781b8c57604a697e
ljhvvs1181 <a2775269@gmail.com>
2025-02-11 15:58:41 +0800

    feature: 新增Wallet Model userId+address 組合index & blocktype enum

--- lib/db.ts
+++ lib/db.ts
@@ -1,20 +1,21 @@
 import mongoose from "mongoose";
 import User from "@/models/User";
 import Exchange from "@/models/Exchange";
+import Wallet from "@/models/Wallet";
 let isConnect = false;
 
 const dbURL = process.env.MONGODB_URI;
 
 /**
  * 連接到 MongoDB 資料庫的異步函數。
- * 
+ *
  * @returns {Promise<mongoose.Connection>} 返回 mongoose 的連接實例。
  * @throws {Error} 如果環境變數 MONGODB_URI 未設置或連接失敗，則拋出錯誤。
- * 
+ *
  * @example
  * ```typescript
  * import { connectDB } from './lib/db';
- * 
+ *
  * (async () => {
  *   try {
  *     const connection = await connectDB();
@@ -36,6 +37,7 @@ const connectDB = async () => {
     await mongoose.connect(dbURL);
     await User.createIndexes();
     await Exchange.createIndexes();
+    await Wallet.createIndexes();
     isConnect = true;
     return mongoose.connection;
   } catch (err) {
--- models/Wallet.ts
+++ models/Wallet.ts
@@ -9,14 +9,18 @@ export interface IWallet extends Document {
   createAt: Date;
 }
 
+const supportedBlockchain = ["EVM"];
+
 const WalletSchema: Schema = new Schema({
   userId: { type: String, required: true },
   label: { type: String, required: true },
   address: { type: String, required: true },
-  blockchain: { type: String, required: true },
+  blockchain: { type: String, enum: supportedBlockchain, required: true },
   type: { type: String, enum: ["hot", "cold"], required: true },
   createAt: { type: Date, default: Date.now },
 });
+
+WalletSchema.index({ userId: 1, address: 1 }, { unique: true });
 
 const Wallet =
   mongoose.models.Wallet || mongoose.model<IWallet>("Wallet", WalletSchema);
