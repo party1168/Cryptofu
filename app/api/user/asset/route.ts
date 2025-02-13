68bc649976d9cf7e04cbbf5d29aada7af1c48f7d
ljhvvs1181 <a2775269@gmail.com>
2025-02-13 14:49:22 +0800

    refactor: 將api/users 修改為api/user

---
+++ app/api/user/asset/route.ts
@@ -0,0 +1,79 @@
+import getAllSpot from "@/lib/api/getAllSpot";
+import getAllWalletBalances from "@/lib/api/getAllWalletBalances";
+import connectDB from "@/lib/database/db";
+import User from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+import { verifyToken } from "@/lib/utils/auth";
+import Wallet from "@/models/Wallet";
+import Exchange from "@/models/Exchange";
+
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("Authorization");
+  if (!authHeader) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Authorization header is required",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const jwtData = await verifyToken(token);
+    await connectDB();
+    const user = await User.findOne({ uuid: jwtData.uuid });
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+    const wallets = await Wallet.find({ userId: jwtData.uuid });
+    const exchanges = await Exchange.find({ userId: jwtData.uuid });
+    const walletBalances = await getAllWalletBalances(wallets);
+    const exchangeBalances = await getAllSpot(exchanges);
+    const balances = {
+      walletBalances,
+      exchangeBalances,
+    };
+    return NextResponse.json(
+      {
+        success: true,
+        data: balances,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: err.message,
+        },
+        {
+          status: 401,
+        }
+      );
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+          message: err,
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+  }
+}
