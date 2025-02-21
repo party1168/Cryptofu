391423a9e65077c032f01f2120e75b32f05df8d7
ljhvvs1181 <a2775269@gmail.com>
2025-02-21 22:08:21 +0800

    feature: 新增calculateCosts Function可以獲取交易紀錄並計算出成本

---
+++ lib/api/calculateCosts.ts
@@ -0,0 +1,48 @@
+import { RawAccountTrade } from "binance";
+
+interface CostResult {
+  symbol: string;
+  totalCost: number;
+  totalQuantity: number;
+  averageCost: number;
+}
+const quoteAssets = ["USDT", "USDC", "FDUSD", "PYUSD"];
+
+const calculateCosts = (transactions: RawAccountTrade[]) => {
+  const costsMap: { [key: string]: CostResult } = {};
+
+  transactions.forEach((transaction: RawAccountTrade) => {
+    const { symbol, quoteQty, qty, isBuyer } = transaction;
+    const quantity = parseFloat(qty.toString());
+    const cost = parseFloat(quoteQty.toString());
+    const baseAsset = symbol.replace(
+      quoteAssets.find((quote) => symbol.endsWith(quote)) || "",
+      ""
+    );
+    if (!costsMap[baseAsset]) {
+      costsMap[baseAsset] = {
+        symbol: baseAsset,
+        totalCost: 0,
+        totalQuantity: 0,
+        averageCost: 0,
+      };
+    }
+    if (isBuyer) {
+      costsMap[baseAsset].totalCost += cost;
+      costsMap[baseAsset].totalQuantity += quantity;
+    } else {
+      costsMap[baseAsset].totalCost -= cost;
+      costsMap[baseAsset].totalQuantity -= quantity;
+    }
+
+    if (costsMap[baseAsset].totalQuantity !== 0) {
+      costsMap[baseAsset].averageCost =
+        costsMap[baseAsset].totalCost / costsMap[baseAsset].totalQuantity;
+    } else {
+      costsMap[baseAsset].averageCost = 0;
+    }
+  });
+  return Object.values(costsMap);
+};
+
+export default calculateCosts;
