87d51e33f1b5b6653b99b107c619b24704d02c57
ljhvvs1181 <a2775269@gmail.com>
2025-02-23 23:39:20 +0800

    feature: 新增TransactionRecords interface 將 Binance 交易紀錄標準化

--- interfaces/exchange.ts
+++ interfaces/exchange.ts
@@ -1,4 +1,17 @@
-export interface TransactionRecords {}
+export interface TransactionRecords {
+  id: string;
+  orderId: string;
+  symbol: string;
+  price: number;
+  quantity: number;
+  side: string;
+  fee: number;
+  feeCurrency: string;
+  timestamp: string;
+  marketType: string;
+  note?: string;
+  createAt?: string;
+}
 export interface ExchangeParams {
   userId: string;
   name: string;
--- lib/api/getBinanceTransaction.ts
+++ lib/api/getBinanceTransaction.ts
@@ -1,15 +1,18 @@
 import { MainClient } from "binance";
 import withRetry from "./withRetry";
 import getBinanceSpot from "./getBinanceSpot";
-
-const getBinanceTransaction = async (APIkey: string, APIsecret: string) => {
+import { TransactionRecords } from "@/interfaces/exchange";
+const getBinanceTransaction = async (
+  APIkey: string,
+  APIsecret: string
+): Promise<TransactionRecords[]> => {
   try {
     const client = new MainClient({ api_key: APIkey, api_secret: APIsecret });
     const spot = await getBinanceSpot(APIkey, APIsecret);
     const assets = spot.assets;
     const quoteAssets = ["USDT", "USDC", "FDUSD"];
     const transactions = await withRetry(async () => {
-      const allTransactions = [];
+      const allTransactions: TransactionRecords[] = [];
       for (const asset of assets) {
         if (quoteAssets.includes(asset.symbol)) {
           continue;
@@ -20,11 +23,26 @@ const getBinanceTransaction = async (APIkey: string, APIsecret: string) => {
             limit: 1000,
           });
           if (transactions.length === 0) continue;
-          allTransactions.push(...transactions);
+          const transactionRecords = transactions.map((transaction) => {
+            return {
+              id: transaction.id.toString(),
+              orderId: transaction.orderId.toString(),
+              symbol: transaction.symbol,
+              price: Number(transaction.price),
+              quantity: Number(transaction.qty),
+              side: transaction.isBuyer ? "BUY" : "SELL",
+              fee: Number(transaction.commission),
+              feeCurrency: transaction.commissionAsset,
+              timestamp: new Date(transaction.time).toISOString(),
+              marketType: "SPOT",
+            };
+          });
+          allTransactions.push(...transactionRecords);
         }
       }
       return allTransactions;
     });
+
     return transactions;
   } catch (err) {
     throw err;
