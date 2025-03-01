53f24dde2aa66bc09b6d5d8970e65ba8e9920052
ljhvvs1181 <a2775269@gmail.com>
2025-03-01 19:31:30 +0800

    feature: 新增查詢Max Twd Deposit function

--- lib/api/getMaxHistory.ts
+++ lib/api/getMaxHistory.ts
@@ -1,9 +1,17 @@
 import { MAX } from "max-exchange-api-node";
+import withRetry from "../utils/withRetry";
 
 const getMaxHistory = async (APIkey: string, APIsecret: string) => {
   const max = new MAX({ accessKey: APIkey, secretKey: APIsecret });
-  const transaction = await max.rest.getDeposits({});
+  const transaction = await withRetry(async () => max.rest.getDeposits({}));
   return transaction;
+};
+export const getMaxTWDDeposit = async (APIkey: string, APIsecret: string) => {
+  const max = new MAX({ accessKey: APIkey, secretKey: APIsecret });
+  const deposit = await withRetry(
+    async () => await max.rest.getDeposits({ currency: "twd" })
+  );
+  return deposit;
 };
 
 export default getMaxHistory;
